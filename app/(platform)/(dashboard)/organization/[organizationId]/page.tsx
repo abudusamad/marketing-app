551931a1e528c60487c30bbe2b4941d33f6b2c30
abudusamad <abudusamed@gmail.com>
2023-11-27 16:56:16 +0000

    making serber side rending for app routing

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+
 const OrganizationIdPage = () => {
-    return (<div>
-        Organization Id Page
-    </div> );
-}
- 
+	async function create(formData: FormData) {
+		"use server";
+		const title = formData.get("title") as string;
+		await db.board.create({
+			data: {
+				title,
+			},
+		});
+	}
+
+	return (
+		<>
+			<form action={create}>
+				<input
+					type="text"
+					id="title"
+					name="title"
+					required
+					placeholder="Organization ID"
+					className="border-black border p-2"
+                />
+                <Button type="submit" size="sm" className="ml-2 pt-4">
+                    submit
+                    
+            </Button>
+			</form>
+		</>
+	);
+};
+
 export default OrganizationIdPage;
