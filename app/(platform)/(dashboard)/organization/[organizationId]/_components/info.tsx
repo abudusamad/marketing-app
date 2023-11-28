955a1c7631c736c1acc47fbfe7d07f1964e03ba3
abudusamad <abudusamed@gmail.com>
2023-11-28 16:31:13 +0000

    Finsihed with making the Info Profile of the page

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { Skeleton } from "@/components/ui/skeleton";
+import { useOrganization } from "@clerk/nextjs";
+import { CreditCard } from "lucide-react";
+import Image from "next/image";
+
+interface InfoProps {
+	isPro?: boolean;
+}
+
+export const Info = ({ isPro }: InfoProps) => {
+	const { organization, isLoaded } = useOrganization();
+	if (!isLoaded) {
+        return (
+            <Info.Skeleton/>
+        );
+	}
+	return (
+		<div className="flex items-center gap-x-4">
+			<div className="w-[60px] h-[60px] relative">
+				<Image
+					fill
+					src={organization?.imageUrl!}
+					alt="Organization Image"
+					className="rounded-md object-cover"
+				/>
+			</div>
+			<div className="space-y-1">
+				<p className="font-semibold text-xl ">{organization?.name}</p>
+				<div className="flex items-center text-xs text-muted-foreground">
+					<CreditCard className="w-4 h-4 mr-1" />
+					Free
+				</div>
+			</div>
+		</div>
+	);
+};
+
+Info.Skeleton = function SkeletonInfo() {
+	return (
+		<div className="flex items-center gap-x-4">
+			<div className="w-[60px] h-[60px] relative">
+				<Skeleton className="w-full h-full absolute" />
+            </div>
+            <div className="space-y-2">
+                <Skeleton className="w-24 h-10" />
+                <div className="flex items-center">
+                    <Skeleton className="w-6 h-4 mr-2" />
+                    <Skeleton className="w-10 h-4" />
+
+                </div>
+
+            </div>
+		</div>
+	);
+};
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,26 +1,17 @@
 
 import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
+import { Info } from "./_components/info";
 
 
 const OrganizationIdPage = async() => {
-    const boards = await db.board.findMany();
+    
 	return (
-		<div className="flex flex-col space-y-4" >
-			<form > 
-				<input 
-					type="text"
-					id="title"
-					name="title"
-					required
-					placeholder="Organization ID"
-					className="border-black border p-2"
-				/>
-				<Button type="submit" size="sm" className="ml-2 pt-4">
-					submit
-				</Button>
-            </form>
-           
+		<div className="w-full mb-20">
+			<Info/>
+			<div>
+
+			</div>
 		</div>
 	);
 };
