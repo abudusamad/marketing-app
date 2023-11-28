22e11f50bc6fd4fb88b11f8bf8a486d2c548bcf5
abudusamad <abudusamed@gmail.com>
2023-11-28 15:57:12 +0000

    Finished with making the server side action for creatting the board for indidual organization

---
+++ actions/create-board/index.tsx
@@ -0,0 +1,56 @@
+"use server";
+
+import { createSafeAction } from "@/lib/create-safe-action";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { revalidatePath } from "next/cache";
+import { CreateBoard } from "./schema";
+import { InputType } from "./types";
+
+const handler = async (data: InputType) => {
+	const { userId, orgId } = auth();
+
+	if (!userId || !orgId) {
+		throw new Error("Unauthorized");
+	}
+
+	const { title, image } = data;
+
+	const [imageId, imageThumbUrl, imageFullUrl, imageLinkHTML, imageUserName] =
+		image.split("|");
+
+	if (
+		!imageId ||
+		!imageThumbUrl ||
+		!imageFullUrl ||
+		!imageLinkHTML ||
+		!imageUserName
+	) {
+		return {
+			error: "Missing fields. Failed to create board",
+		};
+	}
+	let board;
+
+	try {
+		board = await db.board.create({
+			data: {
+				title,
+				orgId,
+				imageId,
+				imageThumbUrl,
+				imageFullUrl,
+				imageLinkHTML,
+				imageUserName,
+			},
+		});
+	} catch (error) {
+		return {
+			error: "Failed to create board",
+		};
+	}
+	revalidatePath(`/boards/${board.id}`);
+	return { data: board };
+};
+
+export const createBoard = createSafeAction(CreateBoard, handler);
