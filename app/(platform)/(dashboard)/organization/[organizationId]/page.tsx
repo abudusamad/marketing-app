f44a12cb939c8e48daac0551cdf33609488aa362
abudusamad <abudusamed@gmail.com>
2023-11-28 12:52:47 +0000

    cleaning the code

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,14 +1,14 @@
-import { create } from "@/actions/create-board";
+
 import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
-import { Board } from "./Board";
+
 
 const OrganizationIdPage = async() => {
     const boards = await db.board.findMany();
 	return (
 		<div className="flex flex-col space-y-4" >
-			<form action={create}>
-				<input
+			<form > 
+				<input 
 					type="text"
 					id="title"
 					name="title"
@@ -20,10 +20,7 @@ const OrganizationIdPage = async() => {
 					submit
 				</Button>
             </form>
-            {boards.map((board) => (
-                <Board key={board.id} title={board.title} id={board.id} />
-            ))}
-            
+           
 		</div>
 	);
 };
