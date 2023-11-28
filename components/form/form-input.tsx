c417a241fda4dcf4c84671815649ea0be5fce3c1
abudusamad <abudusamed@gmail.com>
2023-11-28 14:41:04 +0000

    making the forminput and installing the necessary packages from shadcn ui

---
+++ components/form/form-input.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { forwardRef } from "react";
+import { useFormStatus } from "react-dom";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+
+interface FormInputProps {
+	id: string;
+	label?: string;
+	type?: string;
+	placeholder?: string;
+	required?: boolean;
+	disabled?: boolean;
+	error?: Record<string, string[] | undefined>;
+	className?: string;
+	defaultValue?: string;
+	onBlur?: () => void;
+}
+
+export const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
+	(
+		{
+			id,
+			label,
+			type,
+			placeholder,
+			required,
+			disabled,
+			error,
+			className,
+			defaultValue = "",
+			onBlur,
+		},
+		ref
+	) => {
+		const { pending } = useFormStatus();
+		return (
+			<div className="space-y-2">
+				<div className="space-y-1">
+					{label ? (
+						<Label
+							htmlFor={id}
+							className="text-sm font-semibold text-neutral-700"
+						>
+							{label}
+						</Label>
+					) : null}
+					<Input
+						onBlur={onBlur}
+						defaultValue={defaultValue}
+						id={id}
+						ref={ref}
+						type={type}
+						placeholder={placeholder}
+						required={required}
+						disabled={pending || disabled}
+						name={id}
+						className={cn("text-sm px-2 py-1 h-7", className)}
+						aria-describedby={`${id}-error`}
+					/>
+				</div>
+			</div>
+		);
+	}
+);
+
+FormInput.displayName = "FormInput";
