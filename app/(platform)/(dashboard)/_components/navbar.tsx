61b60550da477f2a77f04deb316a5aa3a73768e0
abudusamad <abudusamed@gmail.com>
2023-11-29 00:00:50 +0000

    fnished with the popover fo the board

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,4 +1,5 @@
 import { Logo } from "@/components/Logo";
+import { FormPopover } from "@/components/form/form-popover";
 import { Button } from "@/components/ui/button";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Plus } from "lucide-react";
@@ -7,25 +8,29 @@ import MobileSidebar from "./mobile-sidebar";
 const Navbar = () => {
 	return (
 		<nav className="fixed top-0 z-50 h-14 px-4 border-b w-full bg-white flex items-center shadow-sm ">
-			<MobileSidebar/>
+			<MobileSidebar />
 			<div className="flex items-center gap-x-4">
 				<div className="hidden md:flex">
 					<Logo />
 				</div>
-				<Button
-					size="sm"
-					variant="primary"
-					className="rounded-md hidden md:block h-auto py-1.5 px-2"
-				>
-					create
-				</Button>
-				<Button
-					variant="primary"
-					size="sm"
-					className="rounded:md block md:hidden "
-				>
-					<Plus className="h-4 w-4" />
-				</Button>
+				<FormPopover align="start" side="bottom" sideOffset={18}>
+					<Button
+						size="sm"
+						variant="primary"
+						className="rounded-md hidden md:block h-auto py-1.5 px-2"
+					>
+						create
+					</Button>
+				</FormPopover>
+				<FormPopover>
+					<Button
+						variant="primary"
+						size="sm"
+						className="rounded:md block md:hidden "
+					>
+						<Plus className="h-4 w-4" />
+					</Button>
+				</FormPopover>
 			</div>
 			<div className="ml-auto flex items-center gap-x-2">
 				<OrganizationSwitcher
