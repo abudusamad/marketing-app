cd1f05523b9fa3ade771ecae11288f2d6146c460
abudusamad <abudusamed@gmail.com>
2023-12-02 02:39:34 +0000

    Finishd withe the copy and deleting of the card

---
+++ actions/copy-card/schema.ts
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+export const CopyCard = z.object({
+
+	id: z.string(),
+	boardId: z.string(),
+});
--- components/modals/card-modal/actions.tsx
+++ components/modals/card-modal/actions.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { copyCard } from "@/actions/copy-card";
 import { deleteCard } from "@/actions/delete-card";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
@@ -15,37 +16,70 @@ interface ActionsProps {
 }
 
 export const Actions = ({ data }: ActionsProps) => {
-    const params = useParams();
-    const cardModal = useCardModal();
+	const params = useParams();
+	const cardModal = useCardModal();
 
-    const { execute: executeDeleteCard, isLoading: isLoadingDelete } = useAction(deleteCard, {
-        onSuccess: (data) => {
-            toast.success(`Card "${data.title}" deleted`);
-            cardModal.onClose();
-        },
-        onError: (error) => {
-            toast.error(error);
-        }
-        
-    })
+	const { execute: executeDeleteCard, isLoading: isLoadingDelete } = useAction(
+		deleteCard,
+		{
+			onSuccess: (data) => {
+				toast.success(`Card "${data.title}" deleted`);
+				cardModal.onClose();
+			},
+			onError: (error) => {
+				toast.error(error);
+			},
+		}
+	);
 
-    const onDelete = () => {
-        const boardId = params.boardId as string;
+	const { execute: executeCopy, isLoading: isLoadingCopy } = useAction(
+		copyCard,
+		{
+			onSuccess: (data) => {
+				toast.success(`Card "${data.title}" copied`);
+				cardModal.onClose();
+			},
+			onError: (error) => {
+				toast.error(error);
+			},
+		}
+	);
 
-        executeDeleteCard({
-            id: data.id,
-            boardId
-        })
-    }
+	const onDelete = () => {
+		const boardId = params.boardId as string;
+
+		executeDeleteCard({
+			id: data.id,
+			boardId,
+		});
+	};
+	const onCopy = () => {
+		const boardId = params.boardId as string;
+
+		executeCopy({
+			id: data.id,
+			boardId,
+		});
+	};
 
 	return (
 		<div className="space-y-2 mt-2">
 			<p className="text-xs font-semibold">Actions</p>
-			<Button  variant="gray" className="w-full justify-start">
+			<Button
+				onClick={onCopy}
+				disabled={isLoadingCopy}
+				variant="gray"
+				className="w-full justify-start"
+			>
 				<Copy className="h-4 w-4 mr-2" />
 				Copy
 			</Button>
-			<Button onClick={onDelete} disabled={isLoadingDelete} variant="gray" className="w-full justify-start">
+			<Button
+				onClick={onDelete}
+				disabled={isLoadingDelete}
+				variant="gray"
+				className="w-full justify-start"
+			>
 				<Trash className="h-4 w-4 mr-2" />
 				Delete
 			</Button>
@@ -54,11 +88,11 @@ export const Actions = ({ data }: ActionsProps) => {
 };
 
 Actions.Skeleton = function ActionSkeleton() {
-    return (
-			<div className="space-y-2 mt-2">
-				<Skeleton className="w-20 h-4 bg-neutral-200" />
-				<Skeleton className="w-full h-8 bg-neutral-200" />
-				<Skeleton className="w-full h-8 bg-neutral-200" />
-			</div>
-		);
-}
+	return (
+		<div className="space-y-2 mt-2">
+			<Skeleton className="w-20 h-4 bg-neutral-200" />
+			<Skeleton className="w-full h-8 bg-neutral-200" />
+			<Skeleton className="w-full h-8 bg-neutral-200" />
+		</div>
+	);
+};
