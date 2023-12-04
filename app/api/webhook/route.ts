5f540cdd1c5b26ebabf5c2ef8a35964caa5b5280
abudusamad <abudusamed@gmail.com>
2023-12-04 11:31:44 +0000

    Adding stripe webhook to the api for stripe subscription

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,6 +1,6 @@
-import Stripe from "stripe";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
+import Stripe from "stripe";
 
 import { db } from "@/lib/db";
 import { stripe } from "@/lib/stripe";
