552d9e3c28e349ce104f5fa6eb3953a24910a835
abudusamad <abudusamed@gmail.com>
2023-12-04 12:10:34 +0000

    Adding the billing for pro or free board and manage subscription

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/billing/_components/subscription-button.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import { stripeRedirect } from "@/actions/stripe-redirect";
+import { Button } from "@/components/ui/button";
+import { useAction } from "@/hooks/use-action";
+import { useProModal } from "@/hooks/use-pro-modal";
+import { toast } from "sonner";
+
+export const SubscriptionButton = ({ isPro }: { isPro: boolean }) => {
+    const proModal = useProModal();
+
+    const { execute, isLoading } = useAction(stripeRedirect, {
+        onSuccess: (data) => {
+            window.location.href = data;
+        },
+        onError: (error) => {
+            toast.error(error);
+        }
+    })
+    
+    const onSubmit = () => {
+        if (isPro) {
+            execute({});
+        } else {
+            proModal.onOpen();
+        }
+    }
+    
+    
+    return (
+        <Button
+            onClick={onSubmit}
+            disabled={isLoading}
+            variant="primary"
+        >
+            {isPro ? "Manage subscription" : "Upgrade to Pro"}
+        </Button>
+    )
+};
